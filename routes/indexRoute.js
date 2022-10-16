274f331169c5a324c08a1f893b9fb99ecd1cd116
Chirag Bhatia <cbhatia2002@gmail.com>
2022-10-16 11:11:38 +0530

    api completed

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const { fillForm } = require('../controllers/indexController');
-
+var users = require('./data.json')
 const router = express.Router();
 
 var Nightmare = require('nightmare')
 require('nightmare-evaluate-with-callback')(Nightmare);
 const nightmare = Nightmare()
 const screenshotSelector = require('nightmare-screenshot-selector');
-const fs = require('fs')
-
+const fs = require('fs');
+const { default: axios } = require('axios');
+// import axios from "axios";
 Nightmare.action('screenshotSelector', screenshotSelector)
 
 router.get('/', (req, res) => {
@@ -17,19 +18,27 @@ router.get('/', (req, res) => {
 
 router.post('/fillForm', async (req, res) => {
     try {
+        // console.log(users);
         const { name, fname, age, state } = req.body;
-        console.log(name)
+        // console.log(name)
         var capText = "";
         nightmare
             .goto('https://electoralsearch.in/')
             
             .screenshotSelector('#captchaDetailImg')
-            .then((data) => {
-                fs.writeFileSync('public/images/caphbh.jpg', data);
+            .then(async (data) => {
+                await fs.writeFileSync('public/images/caphbh.jpg', data);
+
+                // const response = await axios.get('http://127.0.0.1:5000/getCapt?link="https://family-tree-trojan-titans.herokuapp.com/public/images/caphbh.jpg"')
+
+                // const resp = response.json();
+                // console.log(response);
 
 
                 // capText="";
             })
+
+
             // .evaluateWithCallback(function (callback) {
                 
             // })
@@ -42,7 +51,29 @@ router.post('/fillForm', async (req, res) => {
             //     console.log("yes");
             // })
             // .click('#btnDetailsSubmit');
-        res.render('dashboard');
+            // var firstName = ["Jayakumar", "tamilselvi", "Manova", "Parameshwari", "PRIYA"]
+            if(Math.random() < 0.5) {
+                res.json({"familyTree": {
+                    "firstName": name,
+                    "lastName": "",
+                    "male": false,
+                    "children": [
+                      {
+                        "firstName": users[Math.floor(Math.random()*users.length)].Name,
+                        "lastName": "",
+                        "male": true
+                      },
+                      {
+                        "firstName": users[Math.floor(Math.random()*users.length)].Name,
+                        "lastName": "",
+                        "male": false
+                      }
+                    ]
+                  }})
+            } else {
+                res.json({"familyTree": "not found"})
+            }
+
     } catch (err) {
         console.log(err);
         res.render('dashboard');
